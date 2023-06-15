a6c6da54e9fa21d1b200c1e138780df59422de33
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-06-15 04:12:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,6 @@
 */
 int main(void)
 {
-	puts(*\*Programming is like building a multilingual puzzle*);
+	puts(*\"Programming is like building a multilingual puzzle");
 	return (0);
 }
