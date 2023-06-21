deb46bf40bbe53f529954790660ccfb2526a06ce
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-06-21 07:17:32 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,23 +7,22 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	long int i, j, k, next;
 
-	while (i < 58)
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; i++)
 	{
-		if (i == 1)
-			printf("%ld", j);
-		else if (i == 2)
-			 printf(",%ld", k);
-		else
+		if (j != 20365011074)
 		{
-			k += j;
-			j = k = j;
-			printf(",%ld", k);
+			printf("%ld,",j);
+		}else
+		{
+			printf("%ld\n",j);
 		}
-		i++;
+		next = j + k;
+		j = k;
 	}
-	printf("\n");
 	return (0);
 }
