13c7d0e7245eb3a1b46c69645296221212cdbfdd
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-06-21 10:46:33 -0700

    vi 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{       
+        int i = 0;
+        unsigned long int a = 0; b = 1; next = 0;
+        
+        while (i < 98)
+        {       
+                next = a + b;
+                a = b;
+                b = next;
+                printf("%lu", next);
+
+		if (i < 97)
+			printf(",");
+		i++;
+	}
+	putchar("\n");
+	return (0);
+}
