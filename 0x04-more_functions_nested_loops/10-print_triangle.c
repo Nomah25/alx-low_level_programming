29278450d088f17318aecc4ad9a3cc83c2a2ece8
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-06-22 12:33:59 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ * Description: Prints diagonals
+ * @size: size of the triangle
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (c = size - a; c >= 1; c --)
+			{
+				_putchar(' ');
+			}
+			for (b = 1; b <= a; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
