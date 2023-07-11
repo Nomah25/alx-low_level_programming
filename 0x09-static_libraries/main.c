2c125c3afb97c60a47e77fe1de10cae65493e8f5
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-07-11 14:43:13 -0700

    main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
-			    _puts("\"-Kevin Mitnick\"\n\t");
+			    _puts("-Kevin Mitnick\n");
 	return (0);
 }
