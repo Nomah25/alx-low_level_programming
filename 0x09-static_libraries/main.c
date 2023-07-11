8a25e74862572ea3fb227c520918fc37381e594a
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-07-11 14:38:29 -0700

    main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * Function initializes program
+ * Return: 0 Alwayd (Success)
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+			    _puts("\"-Kevin Mitnick\"\n\t");
+	return (0);
 }
