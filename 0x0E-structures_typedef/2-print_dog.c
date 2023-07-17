2ea7d34117dc998d2a37544a2d945f6fe1110020
Nomanguni Khumalo <sebenzagurl19@gmail.com>
2023-07-17 10:29:46 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,5 +16,5 @@ void print_dog(struct dog *d)
 		d->owner = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	  printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	  printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
